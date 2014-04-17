d36cabfd8c87636fe9abae0c443241143874db6c
Nick Doiron <ndoiron@mapmeld.com>
2014-04-16 21:27:58 -0500

    no chrome-specific jquery.ime file

--- chrome-app/background.js
+++ chrome-app/background.js
@@ -2,7 +2,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('../chrome-app/index.html', {
     'bounds': {
       'width': 800,
-      'height': 500
+      'height': 700
     }
   });
 });
