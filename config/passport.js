5b2d4705c5629a8dd4522cfcdc3daa147676f7e3
Nick Doiron <ndoiron@mapmeld.com>
2014-04-22 23:17:44 -0500

    fix module path

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 var LocalStrategy = require('passport-local').Strategy;
-var User = require('../app/models/user');
+var User = require('../models/user');
 
 module.exports = function(passport){
   passport.serializeUser(function(user, done){
