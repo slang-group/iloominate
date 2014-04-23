9f2c4211b9196086704f217c6136b9f1bc868d9e
Nick Doiron <ndoiron@mapmeld.com>
2014-04-22 23:41:44 -0500

    fix login

--- app.js
+++ app.js
@@ -47,12 +47,10 @@ app.get('/login', function(req, res){
   });
 });
 
-app.post('/login', function(req, res){
-  app.post('/login', passport.authenticate('local-login', {
-    successRedirect: '/profile',
-	failureRedirect: '/login',
-	failureFlash: true
-  }));
+app.post('/login', passport.authenticate('local-login', {
+  successRedirect: '/profile',
+  failureRedirect: '/login',
+  failureFlash: true
 });
 
 app.get('/signup', function(req, res){
