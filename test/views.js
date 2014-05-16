1d4eb94c062ae35df676a2261eced20a83b42174
Nick Doiron <ndoiron@mapmeld.com>
2014-05-15 20:37:53 -0500

    add language tests to homepage

--- test/views.js
+++ test/views.js
@@ -12,4 +12,27 @@ describe('GET /', function(){
         done();
       });
   });
+  
+  it('should return Spanish translation when requested', function(done){
+    request(app)
+      .get('/?language=es')
+      .expect(200)
+      .end(function(err, res){
+        console.log(res);
+        assert.include(res.text, 'Escriba la primera página');
+        done();
+      });
+  });
+  
+  it('should return first language of accept-language header', function(done){
+    request(app)
+      .get('/')
+      .set('accept-language', 'es_UY')
+      .expect(200)
+      .end(function(err, res){
+        console.log(res);
+        assert.include(res.text, 'Escriba la primera página');
+        done();
+      });
+  });
 });
