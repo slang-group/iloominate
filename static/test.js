867701d83c4023a6bfa3879c15dfcfa301523517
Nick Doiron <ndoiron@mapmeld.com>
2014-05-15 20:28:50 -0500

    larger image area with fading gradient to text

--- static/test.js
+++ static/test.js
@@ -19,38 +19,71 @@ $(document).ready(function () {
 // store page content
 var pages = [{ text: "", image: null }];
 var current_page = 0;
+var current_image = null;
 
-function saveCurrentPage() {
+function saveCurrentPage(callback) {
   var page_text = $("textarea").val();
   pages[current_page].text = page_text;
   $($(".page-list p")[current_page]).text(page_text.substring(0,18));
 
-  if($(".filedrop img").attr("src")){
-    pages[current_page].image = $(".filedrop img").attr("src");
-    pages[current_page].image_width = $(".filedrop img").width();
-    pages[current_page].image_height = $(".filedrop img").height();    
+  if(current_image){
+    pages[current_page].image = current_image;
+    var img = new Image();
+    img.onload = function(){
+      pages[current_page].image_width = img.width;
+      pages[current_page].image_height = img.height;
+      img = null;
+      callback();
+    };
+    img.src = current_image;
+  }
+  else{
+    callback();
   }
 }
 
 function setCurrentPage(p) {
-  saveCurrentPage();
-  
-  current_page = p;
+  saveCurrentPage(function(){
+    current_page = p;
+    current_image = pages[p].image;
 
-  $(".page-list a").removeClass("active");
-  $($(".page-list a")[p]).addClass("active");
+    $(".page-list a").removeClass("active");
+    $($(".page-list a")[p]).addClass("active");
   
-  // set text and highlighting
-  $("textarea").val(pages[p].text);
-  $("textarea").trigger("input");
+    // set text and highlighting
+    $("textarea").val(pages[p].text);
+    $("textarea").trigger("input");
 
-  // set image
-  if(pages[p].image){
-    $(".filedrop img").attr("src", pages[p].image);
-  }
-  else{
-    $(".filedrop img").attr("src", "");
-  }
+    // read page
+    if(current_image){
+      // set up image on page    
+      $(".filedrop")
+        .removeClass("bordered")
+        .html("");
+  
+      $(".image_area")
+        .addClass("fullsize")
+        .css({
+          "background": "-webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0)), color-stop(75%,rgba(0,0,0,0)), color-stop(100%,#fff)), url(" + current_image + ")",
+          "width": $("textarea").width()
+        })
+        .find("h4").hide();
+    }
+    else{
+      // remove any existing image on page
+      $(".filedrop")
+        .addClass("bordered")
+        .html("");
+  
+      $(".image_area")
+        .removeClass("fullsize")
+        .css({
+          "background": "none",
+          "width": "50%"
+        })
+        .find("h4").show();
+    }
+  });
 }
 
 // drop an image onto the page
@@ -62,8 +95,18 @@ var blockHandler = function (e) {
 };
 
 var processImage = function (e) {
-  var img_url = e.target.result;
-  $(".filedrop").removeClass("bordered").html("").append($("<img/>").attr("src", img_url));
+  current_image = e.target.result;
+  $(".filedrop")
+    .removeClass("bordered")
+    .html("");
+  
+  $(".image_area")
+    .addClass("fullsize")
+    .css({
+      "background": "-webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0)), color-stop(75%,rgba(0,0,0,0)), color-stop(100%,#fff)), url(" + current_image + ")",
+      "width": $("textarea").width()
+    })
+    .find("h4").hide();
 };
 
 var processWhitelist = function (e) {
@@ -123,92 +166,94 @@ window.addEventListener('drop', dropFile, false);
 
 // PDF export
 function pdfify() {
-  saveCurrentPage();
+  saveCurrentPage(function(){
 
-  var ctx = $("canvas")[0].getContext("2d");
-  ctx.font = "20px sans-serif";
-  ctx.fillStyle = "#000";
+    var ctx = $("canvas")[0].getContext("2d");
+    ctx.font = "20px sans-serif";
+    ctx.fillStyle = "#000";
 
-  var doc = new jsPDF();
+    var doc = new jsPDF();
 
-  if(rightToLeft){
-    pages.reverse();
-  }
+    if(rightToLeft){
+      pages.reverse();
+    }
 
-  for(var p=0; p<pages.length; p++) {
-    // add user image
-    var img_offset = 0;
-    if(pages[p].image){
-      var insert_img = pages[p].image;
-      img_offset += pages[p].image_height / 8;
-      var img_format = 'PNG';
-      if(pages[p].image.indexOf("data:image/jpeg") === 0){
-        img_format = 'JPEG';
+    for(var p=0; p<pages.length; p++) {
+      // add user image
+      var img_offset = 0;
+      if(pages[p].image){
+        var insert_img = pages[p].image;
+        img_offset += pages[p].image_height / 8;
+        var img_format = 'PNG';
+        if(pages[p].image.indexOf("data:image/jpeg") === 0){
+          img_format = 'JPEG';
+        }
+        doc.addImage(pages[p].image, img_format, 20, 40, pages[p].image_width / 8, pages[p].image_height / 8);
       }
-      doc.addImage(pages[p].image, img_format, 20, 40, pages[p].image_width / 8, pages[p].image_height / 8);
+
+      // add internationalized text as an image
+      $("canvas").attr("width", 500);
+      ctx.fillText(pages[p].text, 0, 40);
+      var imgData = $("canvas")[0].toDataURL();
+      doc.addImage(imgData, 'PNG', 20, 40 + img_offset, 125, 75);
+
+      if(p !== pages.length - 1){
+        // need next page
+        doc.addPage();
+      }
     }
 
-    // add internationalized text as an image
-    $("canvas").attr("width", 500);
-    ctx.fillText(pages[p].text, 0, 40);
-    var imgData = $("canvas")[0].toDataURL();
-    doc.addImage(imgData, 'PNG', 20, 40 + img_offset, 125, 75);
-
-    if(p !== pages.length - 1){
-      // need next page
-      doc.addPage();
+    if(rightToLeft){
+      pages.reverse();
     }
-  }
 
-  if(rightToLeft){
-    pages.reverse();
-  }
-
-  doc.save('Test.pdf');
+    doc.save('Test.pdf');
+  });
 }
 
 $(".pdfify").on("click", pdfify);
 
 // book preview
 function bookify() {
-  saveCurrentPage();
+  saveCurrentPage(function(){
 
-  // generate book preview
-  if(rightToLeft){
-    pages.reverse();
-  }
-  
-  $(".book").html("");
-  for(var p=0;p<pages.length;p++){
-    var page = $("<div>");
-
-    if(pages[p].image){
-      var img = $("<img/>").attr("src", pages[p].image);
-      page.append(img);
+    // generate book preview
+    if(rightToLeft){
+      pages.reverse();
     }
+  
+    $(".book").html("");
+    for(var p=0;p<pages.length;p++){
+      var page = $("<div>");
 
-    var content = $("<span>").text(pages[p].text);
-    page.append(content);
+      if(pages[p].image){
+        var img = $("<img/>").attr("src", pages[p].image);
+        page.append(img);
+      }
 
-    $(".book").append(page);
-  }
-  $(".book").turn({
-    display: 'double',
-    gradients: true,
-    acceleration: true,
-    elevation: 50,
-    peel: 'tr',
-    turnCorners: 'bl,br',
-    corners: 'forward'
-  });
+      var content = $("<span>").text(pages[p].text);
+      page.append(content);
+
+      $(".book").append(page);
+    }
+    $(".book").turn({
+      display: 'double',
+      gradients: true,
+      acceleration: true,
+      elevation: 50,
+      peel: 'tr',
+      turnCorners: 'bl,br',
+      corners: 'forward'
+    });
    
-  // show book preview
-  $($(".container").children()[0]).hide();
-  $(".book").removeClass("hide");
+    // show book preview
+    $($(".container").children()[0]).hide();
+    $(".book").removeClass("hide");
 
-  if(rightToLeft){
-    pages.reverse();
-  }
+    if(rightToLeft){
+      pages.reverse();
+    }
+  });
 }
 $(".bookify").on("click", bookify);
 
@@ -216,11 +261,12 @@ $(".bookify").on("click", bookify);
 var book_id = null;
 
 function upload() {
-  saveCurrentPage();
-  $.post("/book", {pages: pages, book_id: book_id }, function(response) {
-    // redirect to newly created or updated book
-    book_id = response.id;
-    window.location = "/book/" + book_id;
+  saveCurrentPage(function(){
+    $.post("/book", {pages: pages, book_id: book_id }, function(response) {
+      // redirect to newly created or updated book
+      book_id = response.id;
+      window.location = "/book/" + book_id;
+    });
   });
 }
 $(".upload").on("click", upload);
